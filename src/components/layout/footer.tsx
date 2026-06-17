2737659ca97478f2dcd783e1a47bcc630adab1b6
saquelain <saquelain@saquelains-MacBook-Air.local>
2026-06-17 10:48:09 +0530

    push for changes

--- src/components/layout/footer.tsx
+++ src/components/layout/footer.tsx
@@ -103,7 +103,7 @@ export function Footer() {
         </div>
 
         <div className="mt-12 border-t border-white/10 pt-6 text-center text-sm">
-          © {new Date().getFullYear()} {site.name}. All rights reserved.
+          © {new Date().getFullYear()} {site.name}. All rights reserved
         </div>
       </Container>
     </footer>
